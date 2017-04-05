4dbebde97e2b7f81f0302ae86463fb7521fb49d8
qifx <qifx@mail.taiji.com.cn>
2017-04-05 09:51:54 +0800

    change fib step to 1,2,3

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -4,15 +4,21 @@ import java.util.function.Supplier;
  * Created by qiuzhanghua on 2017/2/7.
  */
 public class FibonacciSupplier implements Supplier<Long> {
-  private long previous = 1;
-  private long current = 2;
-
+  private long previous1 = 1;
+  private long previous2 = 2;
+  private long current = 4;
+  
+  
   @Override
   public Long get() {
-    long p = previous;
-    long next = current + previous;
-    previous = current;
+    long p1 = previous1;
+    long p2 = previous2;
+    long c = current;
+    long next = c + p1 + p2;
+    previous1 = p2;
+    previous2 = c;
     current = next;
-    return p;
+    return p1;
   }
+  
 }
